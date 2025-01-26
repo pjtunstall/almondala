76bad3e3bcc2372174ca341ef0950135c59fdda6
pjunstall <pjtunstall@gmail.com>
2025-01-26 17:48:50 +0000

    TS: rendering with one worker; need to throttle render in key-handlers etc not just in draw

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,26 +1,48 @@
-import init, { calculate_mandelbrot } from "../wasm/almondala.js";
-await init();
+let isWorkerInitialized = false;
+let isRenderPending = false;
+let isRenderScheduled = false;
 export default class Renderer {
     imageData;
     ctx;
+    worker = new Worker(new URL("./worker.js", import.meta.url), {
+        type: "module",
+    });
     constructor(imageData, ctx) {
         this.imageData = imageData;
         this.ctx = ctx;
+        this.worker.onmessage = (event) => {
+            const data = event.data;
+            console.log("Hi, I'm main and I just received a message with data:", data);
+            if (data.type === "init") {
+                console.log("Init message received in main.");
+                isWorkerInitialized = true;
+                // this.draw(2024, new State(17));
+            }
+            if (data.type === "render") {
+                if (!isWorkerInitialized) {
+                    console.error("Worked is not initialized but still has sent us a rendered message. This shouldn't happen.");
+                    return;
+                }
+                this.ctx.putImageData(data.newImageData, 0, 0);
+                isRenderPending = false;
+                // // I need to make state available here and pick a maxIterations. Maybe make draw a method of state.
+                // if (isRenderScheduled) {
+                //   this.draw(1024, new State(17));
+                // }
+            }
+        };
     }
     draw(maxIterations, state) {
-        const width = this.imageData.width;
-        const height = this.imageData.height;
-        const pixels = calculate_mandelbrot(width, height, maxIterations, state.fullMaxIterations, state.mid.x, state.mid.y, state.zoom, state.ratio, state.rFactor, state.gFactor, state.bFactor, state.power, state.grayscale);
-        if (this.imageData.data.length !== pixels.length) {
-            console.error("Lengths out of sync: imageData: ${this.imageData.length}, pixels.length: ${pixels.length}");
+        if (!isWorkerInitialized) {
+            console.error("Request to render before worker is initialized.");
             return;
         }
-        for (let i = 0; i < pixels.length; i++) {
-            this.imageData.data[i] = pixels[i];
+        if (isRenderPending) {
+            isRenderScheduled = true;
+            return;
         }
-        this.ctx.putImageData(this.imageData, 0, 0);
-        const { x, y } = state.mid;
-        console.log(`zoom: ${state.zoom}, center: ${x} ${y < 0 ? "-" : "+"} ${Math.abs(y)}i`);
+        isRenderPending = true;
+        this.worker.postMessage({ type: "render", maxIterations, state });
     }
 }
 //# sourceMappingURL=draw.js.map
---
+++ public/js/worker.js
@@ -0,0 +1,46 @@
+import init, { calculate_mandelbrot } from "../../public/wasm/almondala.js";
+// let offscreen: OffscreenCanvas;
+// let ctx: CanvasRenderingContext2D | null = null;
+let isInitialized = false;
+init().then(() => {
+    console.log("wasm initialized");
+    self.postMessage({ type: "init" });
+    onmessage = function (message) {
+        const data = message.data;
+        console.log(data);
+        console.log(data.state.imageData);
+        console.log(data.state.imageData.width);
+        console.log("I'm the worker and I am initialized, so I'm going to calculate.");
+        const maxIterations = data.maxIterations;
+        const state = data.state;
+        let imageData = state.imageData;
+        const { width, height } = imageData;
+        const { fullMaxIterations, mid, zoom, ratio, rFactor, gFactor, bFactor, power, grayscale, } = state;
+        const pixels = calculate_mandelbrot(width, height, maxIterations, fullMaxIterations, mid.x, mid.y, zoom, ratio, rFactor, gFactor, bFactor, power, grayscale);
+        console.log("I believed I've calculated, pixels:", pixels);
+        const clampedPixels = new Uint8ClampedArray(pixels.buffer);
+        const newImageData = new ImageData(clampedPixels, width, height);
+        createImageBitmap(newImageData).then((imageBitmap) => {
+            postMessage({ type: "render", width, height, newImageData }, [
+                newImageData.data.buffer,
+            ]);
+        });
+        // if (imageData.data.length !== pixels.length) {
+        //   console.error(
+        //     "Lengths out of sync: imageData: ${this.imageData.length}, pixels.length: ${pixels.length}"
+        //   );
+        //   return;
+        // }
+        // for (let i = 0; i < pixels.length; i++) {
+        //   imageData.data[i] = pixels[i];
+        // }
+        // // ctx.putImageData(imageData, 0, 0);
+        // const { x, y } = state.mid;
+        // console.log(
+        //   `zoom: ${state.zoom}, center: ${x} ${y < 0 ? "-" : "+"} ${Math.abs(y)}i`
+        // );
+        // // const bitmap = offscreen.transferToImageBitmap();
+        // self.postMessage({ type: "rendered", pixels }, [pixels.buffer]);
+    };
+});
+//# sourceMappingURL=worker.js.map
