06dc9b58fa5c7c00ee0e6640d4be6de7f104562b
pjunstall <pjtunstall@gmail.com>
2025-01-26 00:46:24 +0000

    start on grayscale

--- src/ts/mandelbrot-explorer.ts
+++ src/ts/mandelbrot-explorer.ts
@@ -22,7 +22,7 @@ export default class MandelbrotExplorer {
   renderer!: Renderer;
 
   constructor() {
-    this.state = new State(false);
+    this.state = new State(true); // Begin with `grayscale` true.
     this.renderer = reset(this.canvas, this.ctx, this.state);
 
     if (!this.renderer) {
