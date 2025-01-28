9d7a80deea9eff2b45b6555420d9242e632a11b8
pjunstall <pjtunstall@gmail.com>
2025-01-28 13:36:31 +0000

    removed firstPassIterations variable

--- src/ts/event-handlers/key-handlers.ts
+++ src/ts/event-handlers/key-handlers.ts
@@ -8,7 +8,6 @@ const keys: { [key: string]: boolean } = {};
 export function handleKeys(
   timestamp: number,
   maxIterations: number,
-  firstPassMaxIterations: number,
   canvas: HTMLCanvasElement,
   ctx: CanvasRenderingContext2D,
   renderer: Renderer,
@@ -16,15 +15,7 @@ export function handleKeys(
 ) {
   // The reason for using this key-handling mechanism--an asynchronous loop and `keys` object, rather than simply handling keypresses individually as each one occurs--is so that we can detect key chords (multiple simultaneous keypresses). It also allows us to throttle draw requests in one place. An object is used rather than a set because we don't just want to know if a key is currently down (in which case it's value will be `true`), but also whether it's been pressed and released since the previous iteration (in which case it will be `false`).
   requestAnimationFrame((timestamp) =>
-    handleKeys(
-      timestamp,
-      maxIterations,
-      firstPassMaxIterations,
-      canvas,
-      ctx,
-      renderer,
-      state
-    )
+    handleKeys(timestamp, maxIterations, canvas, ctx, renderer, state)
   );
   if (timestamp - prev < 128) {
     return;
